85df96508898686212e506d5b8916c61aaa9f9c5
esignael <cem.gvl@gmail.com>
2023-05-14 20:42:32 +0200

    Finalized Merkle Tree class

--- StoopidCoin.py
+++ StoopidCoin.py
@@ -44,14 +44,90 @@ class MerkleTree ():
     left: MerkleTree = None
     right: MerkleTree = None
     value: str = None
+    depth: int = 1
 
-    def add (self, leaf):
-        new = MerkleTree()
-        new.left = self
-        self.parent = new
-        self.value = leaf
-        self = new
-        return self, new
+    def __post_init__ (self):
+        pass
+
+    def __copy__ (self):
+        return MerkleTree(**vars(self))
+
+    def __expand__ (self):
+        if self.is_leaf():
+            return False
+        new_arm = MerkleTree(parent=self, depth=self.depth - 1)
+        if self.left is None:
+            self.left = new_arm
+            return True
+        if self.right is None:
+            self.right = new_arm
+            return True
+        return False
+
+    def __getitem__ (self, index):
+        node_value = pow(2, self.depth-1)
+        if index >= (2 * node_value):
+            raise IndexError('out of bounds')
+        if self.is_leaf():
+            return self.value
+        if index < node_value:
+            if self.left is None:
+                raise IndexError('no index left')
+            return self.left[index]
+        if self.right is None:
+            raise IndexError('no index right')
+        return self.right[index % node_value]
+
+    def append (self, value):
+        if self.is_leaf():
+            if self.value is None:
+                self.value = value
+                return True
+            return False
+        self.__expand__()
+        if not self.is_root():
+            return self.left.append(value) or self.right.append(value)
+        if self.left.append(value) or self.right.append(value):
+            return
+        self.left = self.__copy__()
+        self.left.parent = self
+        self.right = None
+        self.left.__finalize__()
+        self.depth += 1
+        self.append(value)
+
+    @staticmethod
+    def int_ (value):
+        return int.from_bytes(value, 'big')
+
+    @staticmethod
+    def bytes_ (value):
+        return value.to_bytes(32, 'big')
+
+    def __hash__ (self):
+        sha = hl.sha512()
+        if self.is_leaf():
+            sha.update(str(self.value).encode())
+            return self.int_(sha.digest())
+        if self.value is not None:
+            return self.value
+        sha.update(self.bytes_(hash(self.left)))
+        if self.right is not None:
+            sha.update(self.bytes_(hash(self.right)))
+            return self.int_(sha.digest())
+        sha.update(self.bytes_(hash(self.left)))
+        return self.int_(sha.digest())
+
+    def __finalize__ (self):
+        if self.value is not None:
+            return True
+        self.left.__finalize__() and self.right.finalize()
+        self.value = hash(self)
+        return True
 
     def is_leaf (self):
-        return not isinstance(self.left, MerkleTree)
+        return self.depth <= 0 
+    
+    def is_root (self):
+        return self.parent is None 
+
