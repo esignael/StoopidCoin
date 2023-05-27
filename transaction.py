d39280fdf6f0a9362b9be3d1dbbb97e66f7434ac
esignael <cem.gvl@gmail.com>
2023-05-27 21:53:05 +0200

    Added Falcon2 to the transactions class

--- transaction.py
+++ transaction.py
@@ -1,4 +1,5 @@
 import dilithium.dilithium as dl
+import falcon.falcon as fl
 from enum import Enum
 from dataclasses import dataclass
 
@@ -15,7 +16,8 @@ class Transaction ():
     def backend (self):
         schemes = {'D2': dl.Dilithium2,
                    'D3': dl.Dilithium3,
-                   'D5': dl.Dilithium5
+                   'D5': dl.Dilithium5,
+                   'F2': fl.Falcon2,
                    }
         return schemes[self.scheme]
 
@@ -36,6 +38,16 @@ if __name__ == '__main__':
     pk, sk = dl.Dilithium5.keygen()
     m =b'wow, that really works'
     a = Transaction(pk, pk, 'D5', 10.5)
-    print(a.message())
-    print(a.sign(sk))
-    print(a.verify())
+    #print(a.message())
+    pk1, sk1 = fl.Falcon2.keygen()
+    r = fl.SecretKey(2)
+    sk2 = [r.f,r.g,r.F,r.G]
+    print('now')
+    b = fl.Falcon2.sign(sk2, m)
+    print(b)
+    print(r.verify(m, b))
+    exit()
+    print(pk1)
+    print(sk1)
+    #print(a.sign(sk))
+    #print(a.verify())
